11c56a59c846ffa03d65088dc7fdb18bc88cb9b8
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-02-21 19:53:42 +0100

    Testing the prompt function

---
+++ prompt.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 18:42:45 by melkholy          #+#    #+#             */
+/*   Updated: 2023/02/21 18:58:08 by melkholy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	const char	prompt[] = "\033[35mminihell\033[36m$\033[0m ";
+	char		*str;
+
+	str = readline(prompt);
+	printf("Here is %s\n", str);
+	free(str);
+}
