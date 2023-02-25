17ed73638ab2264a3a6c021bfc083adbe4922535
bek <bekchanboyusmanv19@gmail.com>
2023-02-25 17:45:15 +0100

    builtint_pwd is done. just need to change some stuff

--- built_ins/ft_pwd.c
+++ built_ins/ft_pwd.c
@@ -1,20 +1,48 @@
-#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h> // for the exit() function
+#include <unistd.h> // for the getcwd() function
 
-void		ft_pwd(void)
+// function to free a dynamically allocated string and set its pointer to NULL
+void ft_free_str(char **str)
 {
-	char	*pwd;
-	int		ret;
-	int exit_status;
-
-	ret = EXIT_SUCCESS;
-	pwd = getcwd(0, 1024);
-	ft_putendl_fd(pwd, 1);
-	free(pwd);
-	exit_status = 0;
+    if (str && *str) // make sure the pointer is not null and the string exists
+    {
+        free(*str);
+        *str = NULL; // set the pointer to null to avoid a dangling pointer
+    }
 }
 
-int main(void)
+int ft_strlen(char *str)
 {
-    ft_pwd();
-    return 0;
+	int x;
+
+	x = 0;
+	while (str[x])
+		x++;
+	return (x);
+}
+
+int main()
+{
+    char *cwd = NULL;
+
+    // get the current working directory
+    cwd = getcwd(cwd, 0);
+    if (cwd == NULL)
+    {
+        perror("Error");
+        return (EXIT_FAILURE);
+    }
+    else if (printf("%s\n", cwd) != ft_strlen(cwd) + 1) //need to implement my own ft_printf
+    {
+        // if printf fails, free the string and print an error message
+        ft_free_str(&cwd);
+        perror("\nError: printf failed");
+        return (EXIT_FAILURE);
+    }
+
+    // free the string and set the pointer to null
+    ft_free_str(&cwd);
+
+    return (EXIT_SUCCESS);
 }
