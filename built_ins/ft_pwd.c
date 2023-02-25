b39326bbb2ee4eef9226040697c70bc9306a6ac5
Bekchanboy (Bob) Usmanov <114252614+thedatab0y@users.noreply.github.com>
2023-02-25 18:01:16 +0100

    Update ft_pwd.c

--- built_ins/ft_pwd.c
+++ built_ins/ft_pwd.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h> // for the exit() function
-#include <unistd.h> // for the getcwd() function
+#include <stdlib.h> // for the exit()
+#include <unistd.h> // for the getcwd()
 
-// function to free a dynamically allocated string and set its pointer to NULL
+// this will free a dynamically allocated str and set its pointer to NULL
+// make sure the pointer is not null and the string exists
+// at last, set the pointer to null to avoid a dangling pointer
 void ft_free_str(char **str)
 {
-    if (str && *str) // make sure the pointer is not null and the string exists
+    if (str && *str) 
     {
         free(*str);
-        *str = NULL; // set the pointer to null to avoid a dangling pointer
+        *str = NULL; 
     }
 }
 
+// find the len of a string
 int ft_strlen(char *str)
 {
 	int x;
@@ -22,26 +25,28 @@ int ft_strlen(char *str)
 	return (x);
 }
 
+// get the current working directory
+//need to implement my own ft_printf
+// if printf fails, free the string and print an error message
+//at last, free the string and set the pointer to null
 int main()
 {
     char *cwd = NULL;
 
-    // get the current working directory
+    
     cwd = getcwd(cwd, 0);
     if (cwd == NULL)
     {
         perror("Error");
         return (EXIT_FAILURE);
     }
-    else if (printf("%s\n", cwd) != ft_strlen(cwd) + 1) //need to implement my own ft_printf
+    else if (printf("%s\n", cwd) != ft_strlen(cwd) + 1) 
     {
-        // if printf fails, free the string and print an error message
         ft_free_str(&cwd);
         perror("\nError: printf failed");
         return (EXIT_FAILURE);
     }
 
-    // free the string and set the pointer to null
     ft_free_str(&cwd);
 
     return (EXIT_SUCCESS);
