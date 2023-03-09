5279de54bdaae34bac8492ffa2363ef4b01dcbd5
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-03-09 02:51:57 +0100

    Lexer is done with in rediration and here_doc, but without norminette

--- prompt.c
+++ prompt.c
@@ -6,7 +6,7 @@
 /*   By: melkholy <melkholy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 18:42:45 by melkholy          #+#    #+#             */
-/*   Updated: 2023/03/07 02:44:59 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/03/09 02:47:44 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,20 @@
 #include <termios.h>
 #include "./Libft/libft.h"
 #define PROMPT "\033[35mminihell\033[36m$\033[0m "
+#define INPUT 1
+#define HEREDOC 2
+#define OUTPUT 4
+#define APPEND 8
 
 /* Node to store the commands in a linked list */
 typedef struct s_cmds
 {
 	char			*cmd;
 	char			**args;
-	int				to_pipe;
-	int				to_file;
+	char			*from_file;
+	char			*hdocs_end;
+	char			*to_file;
+	int				redirect;
 	struct s_cmds	*next;
 }				t_cmds;
 
@@ -127,7 +133,7 @@ char	**ft_double_realloc(char **str, int old_size, int new_size)
 }
 
 /* Used to parse and create the command with its arguments in a node */
-t_cmds	*ft_parse_cmd(char *in_put)
+t_cmds	*ft_lexer(char *in_put)
 {
 	t_cmds	*cmd;
 	int		count;
@@ -170,6 +176,79 @@ void	ft_free_dstr(char **str)
 	free(str);
 }
 
+int	ft_in_redirection(char *in_put)
+{
+	int	count;
+	int	result;
+
+	count = 0;
+	result = 0;
+	if (!in_put[count])
+		return (0);
+	if (in_put[count] == '<')
+		result = INPUT;
+	if (in_put[count + 1] && in_put[count + 1] == '<')
+		result = HEREDOC;
+	if (!in_put[count + 1] || !in_put[count + 2])
+		printf("\033]35mminihell\033]0m :\
+				syntax error near unexpected token `newline'");
+	return (result);
+}
+
+int	ft_get_args(t_cmds *cmd, char *in_put)
+{
+	int	count;
+	int	len;
+
+	len = 0;
+	count = 0;
+	if (cmd->cmd)
+		len = ft_strlen(cmd->cmd);
+	len += ft_isnspace_indx(&in_put[len]);
+	if (!in_put[len])
+		return (0);
+	cmd->args = (char **)ft_calloc(2, sizeof(char *));
+	while (*(in_put + len))
+	{
+		cmd->args[count] = ft_find_word(in_put + len);
+		len += ft_strlen(cmd->args[count]);
+		count ++;
+		len += ft_isnspace_indx(in_put + len);
+		cmd->args = ft_double_realloc(cmd->args, count + 1, count + 2);
+	}
+	return (0);
+}
+
+t_cmds	*ft_redirect_cmd(char *in_put)
+{
+	t_cmds	*cmd;
+	int		len;
+	int		redirect;
+
+	len = 0;
+	redirect = ft_in_redirection(in_put);
+	len += redirect;
+	if (!in_put[len])
+		return (NULL);
+	cmd = (t_cmds *)ft_calloc(1, sizeof(t_cmds));
+	len += ft_isnspace_indx(&in_put[len]);
+	if (redirect == INPUT)
+	{
+		cmd->to_file = ft_find_word(&in_put[len]);
+		len += ft_strlen(cmd->to_file);
+	}
+	else if (redirect == HEREDOC)
+	{
+		cmd->hdocs_end = ft_find_word(&in_put[len]);
+		len += ft_strlen(cmd->hdocs_end);
+	}
+	len += ft_isnspace_indx(&in_put[len]);
+	cmd->cmd = ft_find_word(&in_put[len]);
+	ft_get_args(cmd, &in_put[len]);
+	cmd->next = NULL;
+	cmd->redirect |= redirect;
+	return (cmd);
+}
 /* Used to split the mutiple commands and create 
  a linked list for them and their arguments */
 t_cmds	*ft_many_cmd(char *in_put)
@@ -181,12 +260,18 @@ t_cmds	*ft_many_cmd(char *in_put)
 
 	many_cmd = ft_split(in_put, '|');
 	count = 0;
-	cmds = ft_parse_cmd(many_cmd[count]);
+	if (many_cmd[count][ft_isnspace_indx(many_cmd[count])] == '<')
+		cmds = ft_redirect_cmd(&many_cmd[count][ft_isnspace_indx(many_cmd[count])]);
+	else
+		cmds = ft_lexer(many_cmd[count]);
 	tmp = cmds;
 	count ++;
 	while (many_cmd[count])
 	{
-		tmp->next = ft_parse_cmd(many_cmd[count]);
+		if (many_cmd[count][ft_isnspace_indx(many_cmd[count])] == '<')
+			tmp->next = ft_redirect_cmd(&many_cmd[count][ft_isnspace_indx(many_cmd[count])]);
+		else
+			tmp->next = ft_lexer(many_cmd[count]);
 		tmp = tmp->next;
 		count ++;
 	}
@@ -209,8 +294,10 @@ void	ft_parse_input(char *in_put)
 		return ;
 	if (ft_strchr(&in_put[count], '|'))
 		cmd = ft_many_cmd(&in_put[count]);
+	else if (in_put[count] == '<')
+		cmd = ft_redirect_cmd(&in_put[count]);
 	else
-		cmd = ft_parse_cmd(&in_put[count]);
+		cmd = ft_lexer(&in_put[count]);
 	/* The rest of the function is for demonstration purposes 
 	  to make sure the lexer is working well*/
 	tmp = cmd;
